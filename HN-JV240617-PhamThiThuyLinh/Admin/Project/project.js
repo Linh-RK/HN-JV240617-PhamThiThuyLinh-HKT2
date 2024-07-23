467de125652d6181bec8cdb3fb288b17407d16d1
Linh Linh <pttlinh101193@gmail.com>
2024-07-23 19:58:25 +0700

    Add files via upload

--- HN-JV240617-PhamThiThuyLinh/Admin/Project/project.js
+++ HN-JV240617-PhamThiThuyLinh/Admin/Project/project.js
@@ -37,13 +37,20 @@ const projectList = JSON.parse(window.localStorage.getItem("projectList"));
 const addProjectForm = document.querySelector(".add-project-form");
 const inputProjectBtn = document.querySelector(".btn-add-project");
 const btnAddForm = document.querySelector(".btn-form-add-project");
-const btnCancelForm = document.querySelector(".btn-form-cancel");
+// const btnCancelForm = document.getElementsByClassName("btn-form-cancel");
 const projectLine = document.getElementById("project-line");
-const inputProject = document.getElementById("input-project");
-const inputImage = document.getElementById("img-project-add");
-const inputTech = document.getElementById("project-tech");
-const inputGithub = document.getElementById("project-github");
-const inputDescription = document.getElementById("description");
+
+const inputProject = document.getElementsByClassName("input-project");
+const inputImage = document.getElementsByClassName("img-project-add");
+console.log(inputImage);
+const inputTech = document.getElementsByClassName("project-tech");
+const inputGithub = document.getElementsByClassName("project-github");
+const inputDescription = document.getElementsByClassName("description");
+console.log(inputProject);
+
+const addForm = document.querySelector(".add-form");
+console.log(addForm);
+const updateForm = document.querySelector(".update-form");
 let formTitle = document.querySelector(".title");
 let idGlobal = null;
 
@@ -95,11 +102,11 @@ function addNewProject() {
   const projectList =
     JSON.parse(window.localStorage.getItem("projectList")) || [];
   // CHECK INPUT VALUE========================
-  let newProject = inputProject.value.trim();
-  let newProjectImage = inputImage.value;
-  let newTech = inputTech.value.trim();
-  let newGithub = inputGithub.value.trim();
-  let newDescription = inputDescription.value.trim();
+  let newProject = inputProject[0].value.trim();
+  let newProjectImage = inputImage[0].value;
+  let newTech = inputTech[0].value.trim();
+  let newGithub = inputGithub[0].value.trim();
+  let newDescription = inputDescription[0].value.trim();
 
   if (
     newProject == "" ||
@@ -120,52 +127,47 @@ function addNewProject() {
     return;
   }
   // TAO OBJECT MOI SAVE LOCAL STORAGE========================
-  if (idGlobal == null) {
-    let id = 1;
-    if (!projectList.length == 0) {
-      id = projectList[projectList.length - 1].id + 1;
-    }
-    const newProjectLine = {
-      id: id,
-      project: newProject,
-      img: newProjectImage,
-      tech: newTech,
-      github: newGithub,
-      description: newDescription,
-    };
-    projectList.push(newProjectLine);
-    window.localStorage.setItem("projectList", JSON.stringify(projectList));
-    addProjectForm.style.display = "none";
-    renderProjectList();
-  } else {
-    const projectList =
-      JSON.parse(window.localStorage.getItem("projectList")) || [];
-    const index = projectList.findIndex((project) => project.id == idGlobal);
-
-    console.log(idGlobal);
-    const newProjectList = projectList.slice(index, 1);
-    console.log(newProjectList);
-    const indexCheck = newProjectList.findIndex(
-      (project) =>
-        project.project.toLowerCase() == inputProject.value.toLowerCase()
-    );
-    if (indexCheck !== -1) {
-      alert("Project đã có");
-    }
-    const updateProjectItem = {
-      id: idGlobal,
-      project: inputProject.value,
-      img: inputImage.value,
-      tech: inputTech.value,
-      github: inputGithub.value,
-      description: inputDescription.value,
-    };
-    projectList.splice(index, 1, updateProjectItem);
-    window.localStorage.setItem("projectList", JSON.stringify(projectList));
-    formTitle.innerHTML = "Thêm mới dự án";
-    idGlobal = null;
-    renderProjectList();
+  // if (idGlobal == null) {
+  let id = 1;
+  if (!projectList.length == 0) {
+    id = projectList[projectList.length - 1].id + 1;
   }
+  const newProjectLine = {
+    id: id,
+    project: newProject,
+    img: newProjectImage,
+    tech: newTech,
+    github: newGithub,
+    description: newDescription,
+  };
+  projectList.push(newProjectLine);
+  window.localStorage.setItem("projectList", JSON.stringify(projectList));
+  addProjectForm.style.display = "none";
+  renderProjectList();
+  // } else {
+  //   const projectList =
+  //     JSON.parse(window.localStorage.getItem("projectList")) || [];
+  //   const index = projectList.findIndex((project) => project.id === idGlobal);
+  //   const newProjectList = projectList.slice(index, 1);
+  //   console.log(newProjectList);
+  //   const indexCheck = newProjectList.findIndex(
+  //     (project) =>
+  //       project.project.toLowerCase() == inputProject.value.toLowerCase()
+  //   );
+  //   if (indexCheck !== -1) {
+  //     alert("Project đã có");
+  //   }
+  //   projectList[index].project = inputProject.value;
+  //   projectList[index].img = inputImage.value;
+  //   projectList[index].tech = inputTech.value;
+  //   projectList[index].github = inputGithub.value;
+  //   projectList[index].description = inputDescription.value;
+
+  //   window.localStorage.setItem("projectList", JSON.stringify(projectList));
+  //   formTitle.innerHTML = "Thêm mới dự án";
+  //   idGlobal = null;
+  //   renderProjectList();
+  // }
 }
 // ==============CLICK CANCEL in FORM THEM==================
 function cancelForm() {
@@ -187,12 +189,33 @@ function updateProjectLine(id) {
   formTitle.innerHTML = "SỬA DỰ ÁN";
   // display form
   addProjectForm.style.display = "block";
+  addForm.style.display = "none";
+  updateForm.style.display = "block";
   // find object in projectList
   const index = projectList.findIndex((project) => project.id === id);
-  inputProject.value = projectList[index].project;
-  inputImage.value = projectList[index].img;
-  inputTech.value = projectList[index].tech;
-  inputGithub.value = projectList[index].github;
-  inputDescription.value = projectList[index].description;
+  console.log(projectList[index].img);
+  inputProject[1].value = projectList[index].project;
+  inputImage[1].value = projectList[index].img;
+  inputTech[1].value = projectList[index].tech;
+  inputGithub[1].value = projectList[index].github;
+  inputDescription[1].value = projectList[index].description;
   idGlobal = projectList[index].id;
+  console.log(inputProject[1].value);
+}
+function updateProject(id) {
+  const projectList =
+    JSON.parse(window.localStorage.getItem("projectList")) || [];
+  const index = projectList.findIndex((project) => project.id === idGlobal);
+  const newProjectLine = {
+    id: idGlobal,
+    project: inputProject[1].value,
+    img: inputImage[1].value,
+    tech: inputTech[1].value,
+    github: inputGithub[1].value,
+    description: inputDescription[1].value,
+  };
+  projectList.splice(index, 1, newProjectLine);
+  window.localStorage.setItem("projectList", JSON.stringify(projectList));
+  addProjectForm.style.display = "none";
+  renderProjectList();
 }
